89e644647a6263cb400f7b2f9a5c2a08ee77285f
Moreno Pradita <morenopradita013@gmail.com>
2026-01-07 13:59:29 +0700

    Table done
    - Table schedule is done

---
+++ js/cellInput.js
@@ -0,0 +1,17 @@
+import { schedule } from "./dataSchedule.js";
+
+const days = Object.keys(schedule);
+const maxRows = Math.max(...days.map((day) => schedule[day].length));
+
+const tableBody = document.getElementById("table-body");
+let tableData = "";
+
+for (let row = 0; row < maxRows; row++) {
+  tableData += `<tr>`;
+  for (const day of days) {
+    tableData += `<td contenteditable=true>${schedule[day][row] ?? ""}`;
+  }
+  tableData += `<tr>`;
+}
+
+tableBody.innerHTML = tableData;
---
+++ js/dataSchedule.js
@@ -0,0 +1,34 @@
+export let schedule = {
+  Monday: [
+    "primary Bench",
+    "dips",
+    "Flies",
+    "lateral raise",
+    "bicep curl",
+    "hammer curl",
+  ],
+  Selasa: ["primary Deadlift", "pull up", "cable row", "tricep press"],
+  Rabu: [
+    "Secondary squat",
+    "Shoulder Press",
+    "Leg Extension",
+    "Hamstring curl",
+    "Abs",
+  ],
+  kamis: ["rest"],
+  Jumat: [
+    "secondary bench",
+    "dips",
+    "Lateral raise",
+    "Bicep curl",
+    "Hammer curl",
+  ],
+  Sabtu: [
+    "secondary deadlift",
+    "tertiary bench",
+    "pull up",
+    "back extension",
+    "Tricep overhead",
+  ],
+  Minggu: ["Primary squat", "Abs", "Leg Extension", "Stiff deadlift"],
+};
